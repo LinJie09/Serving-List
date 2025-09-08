9cb3f8320762ae16e39cc4234d66a990f2e66049
LinJie09 <zxc664231@gmail.com>
2025-09-08 14:36:59 +0800

    add new link for happyness group

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -8,12 +8,26 @@ function App() {
 
   return (
     <div className="container">
-        <h1>我的管理工具</h1>
-        <div className="btn-container">
-          <button className="btn" onClick={() => setTab("expense")}>記帳</button>
-          <button className="btn" onClick={() => setTab("schedule")}>工作服事表</button>
-        </div>
-        {tab === "expense" ? <ExpenseTracker /> : <ServiceSchedule />}
+      <h1>我的管理工具</h1>
+      <p>
+        <a
+          href="https://twilight-lute-6a9.notion.site/DJ-1a086409d6038079beb9f2de9469304f"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link-text"
+        >
+          DJ學生區幸福小組
+        </a>
+      </p>
+      <div className="btn-container">
+        <button className="btn" onClick={() => setTab("expense")}>
+          記帳
+        </button>
+        <button className="btn" onClick={() => setTab("schedule")}>
+          工作服事表
+        </button>
+      </div>
+      {tab === "expense" ? <ExpenseTracker /> : <ServiceSchedule />}
     </div>
   );
 }
